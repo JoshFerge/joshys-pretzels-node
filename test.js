31ef5ebf714eaadcc39f79264d865ea131b17beb
Joshua Ferge <josh@Joshuas-MBP-1.hsd1.ca.comcast.net>
2021-01-13 10:51:36 -0800

    init

---
+++ test.js
@@ -0,0 +1,31 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://f20e48638e134b649bf67de1f91df83f@o504099.ingest.sentry.io/5591628",
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+    console.log('hay')
+  }
+}, 99);
